b136ab2dd78380344370c314f44cc6a0348c4844
rlukassa <19623069@std.stei.itb.ac.id>
2026-01-14 12:05:00 +0700

    feat(talent-growth): Update App component layout and HTML entry point

--- client/src/App.vue
+++ client/src/App.vue
@@ -9,6 +9,8 @@ import { RouterView } from 'vue-router'
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
+
 * {
   margin: 0;
   padding: 0;
@@ -16,7 +18,7 @@ import { RouterView } from 'vue-router'
 }
 
 #app {
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   min-height: 100vh;
